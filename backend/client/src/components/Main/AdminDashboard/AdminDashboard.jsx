497e9b617bdf51d35f3235242d36f35adb2d1d6b
GiancarloCiulla <giancarlociulla.alfano@gmail.com>
2024-11-29 19:54:33 +0100

    final/back

--- backend/client/src/components/Main/AdminDashboard/AdminDashboard.jsx
+++ backend/client/src/components/Main/AdminDashboard/AdminDashboard.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import BlogForm from "../../BlogForm/BlogForm"; // Importa el componente BlogForm
-
+import Home from "../../Main/Home/Home";
 const AdminDashboard = () => {
   return (
     <div>
       <h1>Dashboard de Administrador</h1>
       <p>Bienvenido al panel de control.</p>
       
-   
+      {/* <Home /> */}
       <BlogForm />
+
     </div>
   );
 };
