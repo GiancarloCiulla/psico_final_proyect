f1d040b0bbfca3fe94dbca7db5218436e46caec8
GiancarloCiulla <giancarlociulla.alfano@gmail.com>
2024-12-02 13:48:56 +0100

    final/function

--- backend/client/src/App.jsx
+++ backend/client/src/App.jsx
@@ -34,28 +34,3 @@ const App = () => {
 };
 
 export default App;
-
-// import React from 'react';
-// import { Routes, Route } from 'react-router-dom';
-// import Header from './components/Header';
-// import Footer from './components/Footer';
-// import Main from './components/Main';
-// import Categorias from './components/Main/Categories/Categorias';
-// import Login from './components/Header/Login/LoginForm';
-// import AdminDashboard from './components/Main/AdminDashboard/AdminDashboard';
-// //import './App.css';
-// import Home from './components/Main/Home/Home'
-
-// const App = () => {
-//   return (
-//     <>
-//       <Header />
-//       <Categorias />
-//       {/* <Main /> */}
-//       <Home />
-//       {/* <Footer /> */}
-//     </>
-//   );
-// };
-
-// export default App;
